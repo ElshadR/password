ce348d6d8ed4181b5124e899fce1d1ac8745826d
ElshadR <elshadzr@code.edu.az>
2018-04-27 23:24:50 +0400

    pass

---
+++ password/pass.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace password
+{
+    partial class pass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwordd = new System.Windows.Forms.TextBox();
+            this.reqem = new System.Windows.Forms.Button();
+            this.kicikherif = new System.Windows.Forms.Button();
+            this.boyukherif = new System.Windows.Forms.Button();
+            this.simvol = new System.Windows.Forms.Button();
+            this.minimum8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // passwordd
+            // 
+            this.passwordd.Location = new System.Drawing.Point(132, 102);
+            this.passwordd.Multiline = true;
+            this.passwordd.Name = "passwordd";
+            this.passwordd.Size = new System.Drawing.Size(329, 35);
+            this.passwordd.TabIndex = 0;
+            this.passwordd.TextChanged += new System.EventHandler(this.passwordd_TextChanged);
+            // 
+            // reqem
+            // 
+            this.reqem.BackColor = System.Drawing.Color.White;
+            this.reqem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reqem.Location = new System.Drawing.Point(132, 171);
+            this.reqem.Name = "reqem";
+            this.reqem.Size = new System.Drawing.Size(75, 23);
+            this.reqem.TabIndex = 1;
+            this.reqem.UseVisualStyleBackColor = false;
+            // 
+            // kicikherif
+            // 
+            this.kicikherif.BackColor = System.Drawing.Color.White;
+            this.kicikherif.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kicikherif.Location = new System.Drawing.Point(132, 222);
+            this.kicikherif.Name = "kicikherif";
+            this.kicikherif.Size = new System.Drawing.Size(75, 23);
+            this.kicikherif.TabIndex = 1;
+            this.kicikherif.UseVisualStyleBackColor = false;
+            // 
+            // boyukherif
+            // 
+            this.boyukherif.BackColor = System.Drawing.Color.White;
+            this.boyukherif.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boyukherif.Location = new System.Drawing.Point(132, 274);
+            this.boyukherif.Name = "boyukherif";
+            this.boyukherif.Size = new System.Drawing.Size(75, 23);
+            this.boyukherif.TabIndex = 1;
+            this.boyukherif.UseVisualStyleBackColor = false;
+            // 
+            // simvol
+            // 
+            this.simvol.BackColor = System.Drawing.Color.White;
+            this.simvol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.simvol.Location = new System.Drawing.Point(252, 171);
+            this.simvol.Name = "simvol";
+            this.simvol.Size = new System.Drawing.Size(75, 23);
+            this.simvol.TabIndex = 1;
+            this.simvol.UseVisualStyleBackColor = false;
+            // 
+            // minimum8
+            // 
+            this.minimum8.BackColor = System.Drawing.Color.White;
+            this.minimum8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimum8.Location = new System.Drawing.Point(252, 222);
+            this.minimum8.Name = "minimum8";
+            this.minimum8.Size = new System.Drawing.Size(75, 23);
+            this.minimum8.TabIndex = 1;
+            this.minimum8.UseVisualStyleBackColor = false;
+            // 
+            // pass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 435);
+            this.Controls.Add(this.minimum8);
+            this.Controls.Add(this.simvol);
+            this.Controls.Add(this.boyukherif);
+            this.Controls.Add(this.kicikherif);
+            this.Controls.Add(this.reqem);
+            this.Controls.Add(this.passwordd);
+            this.Name = "pass";
+            this.Text = "pass";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox passwordd;
+        private System.Windows.Forms.Button reqem;
+        private System.Windows.Forms.Button kicikherif;
+        private System.Windows.Forms.Button boyukherif;
+        private System.Windows.Forms.Button simvol;
+        private System.Windows.Forms.Button minimum8;
+    }
+}
---
+++ password/pass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace password
+{
+    public partial class pass : Form
+    {
+        public static char[] passiner = new char[] { };
+        public static List<char> reqemm = new List<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        public static List<char> boyuk = new List<char>() {'Q','W','E','R','T','Y','U','I','O','P', 'A','S','D','F','G', 'H', 'J', 'K','L','Z','X','C','V','B','N','M' };
+        public static List<char> simvoll= new List<char>() {'!','@','#','$','%','^','&','*','(',')','_','-','=','+','/','|',']','[','{','}','"',';',':','>','.','<',',','?','~' };
+        public static List<char> kicik = new List<char>() { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm' };
+
+        public static string st;
+        public pass()
+        {
+            InitializeComponent();
+        }
+
+        private void passwordd_TextChanged(object sender, EventArgs e)
+        {
+            st = passwordd.Text;
+            char[] passiner = st.ToCharArray();
+            reqem.BackColor = Color.White;
+            boyukherif.BackColor = Color.White;
+            simvol.BackColor = Color.White;
+            kicikherif.BackColor = Color.White;
+            minimum8.BackColor = Color.White;
+
+            foreach (var req in reqemm)
+            {
+                
+                for (int i = 0; i < passiner.Length; i++)
+                {
+                    if (req==passiner[i])
+                    {
+                        reqem.BackColor = Color.Red;
+                        break;
+                    }
+                }
+            }
+            foreach (var boy in boyuk)
+            {
+
+                for (int i = 0; i < passiner.Length; i++)
+                {
+                    if (boy == passiner[i])
+                    {
+                        boyukherif.BackColor = Color.Red;
+                        break;
+                    }
+                }
+            }
+            foreach (var sim in simvoll)
+            {
+
+                for (int i = 0; i < passiner.Length; i++)
+                {
+                    if (sim == passiner[i])
+                    {
+                        simvol.BackColor = Color.Red;
+                        break;
+                    }
+                }
+            }
+            foreach (var sim in kicik)
+            {
+
+                for (int i = 0; i < passiner.Length; i++)
+                {
+                    if (sim == passiner[i])
+                    {
+                        kicikherif.BackColor = Color.Red;
+                        break;
+                    }
+                }
+            }
+            if (passiner.Length > 7)
+            {
+                minimum8.BackColor = Color.Red;
+            }
+        }
+    }
+}
